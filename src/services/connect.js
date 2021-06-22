c3de8e063b3eaeb4606eb36a6de905d2712a6867
CarlosEduardoC1 <cnobre.trabalho@gmail.com>
2021-06-22 18:33:52 -0300

    urlngrok

--- src/services/connect.js
+++ src/services/connect.js
@@ -1,5 +1,5 @@
 const axios = require('axios').default;
 
-const connect = axios.create({ baseURL: `http://18.228.172.18:3000` });
+const connect = axios.create({ baseURL: `https://5858921b45f5.ngrok.io` });
 
 module.exports = connect;
